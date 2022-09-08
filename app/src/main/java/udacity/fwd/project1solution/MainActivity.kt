1c3a4b7fdfc1bad943445e3dcdabb9bdc239ddb1
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-08 05:13:33 +0200

    navigation adjusted

--- app/src/main/java/udacity/fwd/project1solution/MainActivity.kt
+++ app/src/main/java/udacity/fwd/project1solution/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import timber.log.Timber
 import udacity.fwd.project1solution.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,13 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
-
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.graph_loginFragment,
+            R.id.graph_welcomeFragment,
+            R.id.graph_instructionsFragment,R.id.graph_shoeListFragment))
 //        val navController = this.findNavController(R.id.myNavHostFragment)
 
-        NavigationUI.setupActionBarWithNavController(this,navController)
+//        NavigationUI.
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
 
     }
