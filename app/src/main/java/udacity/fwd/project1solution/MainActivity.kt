6050edf465ed09e8f4d36c5367be67824f594936
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-08 03:39:17 +0200

    layout adjusted

--- app/src/main/java/udacity/fwd/project1solution/MainActivity.kt
+++ app/src/main/java/udacity/fwd/project1solution/MainActivity.kt
@@ -2,12 +2,35 @@ package udacity.fwd.project1solution
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import timber.log.Timber
+import udacity.fwd.project1solution.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return  navController.navigateUp()//NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
