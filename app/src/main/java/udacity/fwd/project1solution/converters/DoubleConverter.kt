2015aad0281c17d15842c9ed5b928f4a8f05fa06
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-09-12 00:27:52 +0200

    Added DoubleConverter to use in data binding

---
+++ app/src/main/java/udacity/fwd/project1solution/converters/DoubleConverter.kt
@@ -0,0 +1,27 @@
+package udacity.fwd.project1solution.converters
+
+import androidx.databinding.InverseMethod
+
+object DoubleConverter {
+    @InverseMethod("stringToDate")
+    @JvmStatic
+    fun doubleToString(
+        value: Double?
+    ): String {
+        if (value == null || value == 0.0) return ""
+        return value.toString()
+    }
+
+    @JvmStatic
+    fun stringToDouble(
+        value: String
+    ): Double? {
+        // Converts String to double.
+        if (value.isBlank()) return null
+        try {
+            return value.toDouble()
+        } catch (ex: NumberFormatException) {
+            return null
+        }
+    }
+}
